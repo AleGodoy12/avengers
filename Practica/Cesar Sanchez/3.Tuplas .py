c4944e7843fdca8c94a216ced04a1df602f305b0
Arbusta <cesar.sanchez@arbusta.net>
2022-08-13 20:15:44 -0300

    Actualizo repo

---
+++ Practica/Cesar Sanchez/3.Tuplas .py
@@ -0,0 +1,46 @@
+# Tuplas
+
+# 1. Crea una tupla con los meses del año, pide números al usuario, si el numero esta entre 1 y la longitud máxima de la tupla, muestra el contenido de esa posición sino muestra un mensaje de error.
+
+# El programa termina cuando el usuario introduce un cero
+
+
+
+# 2.  Crea una tupla con números, pide un numero por teclado e indica cuantas veces se repite.
+
+
+""" numero = (1, 2, 3, 2, 4, 3, 4, 3, 4, 4, 4, 5, 5, 5, 5)
+
+escribirNumero = int (input ("Escriba un número: "))
+
+# cantidadRepeticiones = numero.count(escribirNumero)
+
+# print (f'El número {escribirNumero}, se repite {numero.count(escribirNumero)} de veces')
+
+print ("El número, ", escribirNumero, " se repite ", numero.count(escribirNumero), " veces") """
+
+
+
+
+# 3. Crea una tupla con valores ya predefinidos del 1 al 10, pide un índice por teclado y muestra los valores de la tupla
+
+""" tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+indice = int (input ("Escribe un índice: "))
+
+valor = tupla [indice]
+
+print (f'La posición {indice} tiene un valor de {valor}' ) """
+
+
+
+
+
+
+
+
+
+""" numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+indc = int(input("Ingrese un indice: "))
+a = numbers[indc]
+print("El indice es " + str(indc) + " y su valor es " + str(a)) """
